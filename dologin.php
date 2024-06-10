b5848cf58b27c888a4a84c11ff168d1091b4dca9
marjasback2bedge <21211256asd0810@gmail.com>
2024-06-10 22:05:16 +0800

    add main pages for login

---
+++ dologin.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+include "conn.php";
+
+if (!$conn->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $conn->error);
+    exit();
+}
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$passwd = $_POST['password'];
+
+if ($name && $passwd) {
+    $sql = "SELECT * FROM user WHERE name='$name' AND password = '$passwd'";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows) {
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        $_SESSION['user_id'] = $row['user_id'];
+        $_SESSION['type'] = $row['type'];
+
+        // Check roles
+        if ($_SESSION['type'] == 0) {
+            $_SESSION['login'] = TRUE;
+            header('Location: admin.php');
+        } else {
+            $_SESSION['login'] = TRUE;
+            
+                // Redirect to a default page if the 'serial_no' parameter is not present
+                header("Location: success.php");
+        }
+
+    } else {
+        $_SESSION['login'] = FALSE;
+        $_SESSION['msg'] = '登入失敗，請確認使用者名稱及密碼!!';
+        header('Location: index.php');
+    }
+} else {
+    $_SESSION['msg'] = '請輸入使用者名稱及密碼!!';
+    //header('Location: login.html');
+}
+//session_unset();
+?>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登入</title>
+    <link rel="stylesheet" href="assets/css/login.css">
+</head>
+
+<body>
+    <div class="box">
+
+        <div class="background">
+            <!-- hehe -->
+        </div>
+        
+        <!-- <?php
+        session_start();
+        $sn = isset($_GET['serial_no']) ? $_GET['serial_no'] : '';
+        echo $sn;
+        ?> -->
+
+        <form action="dologin.php" method="post">
+            <div class="log_in_box">
+                <input type="hidden" name="serial_no" value="<?php echo $sn; ?>">
+                <br><br>
+                <h2>登入您的帳號</h2>
+                <br>
+                <p>使用者名稱：<input type="name" class="only_underline" name="name" required="required" /></p>
+                <p>密碼：<input type="password" class="only_underline" name="password" required="required" /></p>
+                <?php
+                if (isset($_SESSION['msg'])) {
+                    echo "<p><font color='#FF0000'>{$_SESSION['msg']}</font></p>";
+                    unset($_SESSION['msg']);
+                }
+                //session_unset();
+                ?>
+                <br>
+                <button class="login_button" type="submit">登入</button>
+                <br>
+                <a href="signup.php" . target='_self' class='signup_pos'>
+                    沒有帳號嗎? </a>
+            </div>
+        </form>
+
+    </div>
+</body>
+
+</html>
