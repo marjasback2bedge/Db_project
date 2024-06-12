a0847caf0f0bb917f0531609be76bf59ed7c77c3
marjasback2bedge <21211256asd0810@gmail.com>
2024-06-13 00:31:54 +0800

    latest ver admin by Wirra-Wi

--- admin/index.php
+++ admin/index.php
@@ -6,177 +6,320 @@ require_once('inc/header.php')
 
 <header id="header" class="header fixed-top d-flex align-items-center">
 
-<!-- <div class="d-flex align-items-center justify-content-between">
-  <a href="http://localhost/php-lfis/admin" class="logo d-flex align-items-center">
-    <img src="http://localhost/php-lfis/uploads/logo.png?v=1682908055" alt="System Logo">
-    <span class="d-none d-lg-block">PHP - LFIS</span>
-  </a>
-  <i class="bi bi-list toggle-sidebar-btn"></i>
-</div> -->
-<!-- End Logo -->
+  <div class="d-flex align-items-center justify-content-between">
 
-<!-- <div class="search-bar">
-  <form class="search-form d-flex align-items-center" method="POST" action="#">
-    <input type="text" name="query" placeholder="Search" title="Enter search keyword">
-    <button type="submit" title="Search"><i class="bi bi-search"></i></button>
-  </form>
-</div> -->
-<!-- End Search Bar -->
+    <i class="bi bi-list toggle-sidebar-btn"></i>
+  </div>
+  <!-- End Logo -->
 
-<nav class="header-nav ms-auto">
-  <ul class="d-flex align-items-center">
-    <li class="nav-item dropdown pe-3">
+  <!-- <div class="search-bar">
+    <form class="search-form d-flex align-items-center" method="POST" action="#">
+      <input type="text" name="query" placeholder="Search" title="Enter search keyword">
+      <button type="submit" title="Search"><i class="bi bi-search"></i></button>
+    </form>
+  </div> -->
+  <!-- End Search Bar -->
 
-      <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-        <!-- <img src="http://localhost/php-lfis/uploads/avatars/1.png?v=1678760026" alt="Profile" class="rounded-circle"> -->
+  <nav class="header-nav ms-auto">
+    <ul class="d-flex align-items-center">
+      <li class="nav-item dropdown pe-3">
+
+        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown" aria-expanded="false">         
+          <?php 
+          $name = $_SESSION['name'];
+          echo "<span class='d-none d-md-block dropdown-toggle ps-2'>$name\t</span>";
+          ?>
+        </a>
+        <!-- End Profile Iamge Icon -->
+
+        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+          <!-- <li class="dropdown-header">
+            <h6>Adminstrator Admin</h6>
+            <span>Administrator</span>
+          </li>
+          <li>
+            <hr class="dropdown-divider">
+          </li> -->
+
+          <!-- <li>
+            <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+              <i class="bi bi-person"></i>
+              <span>My Profile</span>
+            </a>
+          </li> -->
+          <!-- <li>
+            <hr class="dropdown-divider">
+          </li>
+
+          <li>
+            <a class="dropdown-item d-flex align-items-center" href="http://localhost/php-lfis/admin?page=user">
+              <i class="bi bi-gear"></i>
+              <span>Account Settings</span>
+            </a>
+          </li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
         
-        <?php 
-        include '../conn.php';
-        $name = $_SESSION['name'];
-        echo "<span class='d-none d-md-block dropdown-toggle ps-2'>$name\t</span>";
-        ?>
-      </a>
-      <!-- End Profile Iamge Icon -->
+          <li>
+            <hr class="dropdown-divider">
+          </li> -->
 
-      <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-        <!-- <li class="dropdown-header">
-          <h6>Adminstrator Admin</h6>
-          <span>Administrator</span>
-        </li>
-        <li>
-          <hr class="dropdown-divider">
-        </li> -->
+          <li>
+            <a class="dropdown-item d-flex align-items-center" href="<?= base_url ?>">
+              <i class="bi bi-box-arrow-right"></i>
+              <span>登出</span>
+            </a>
+          </li>
 
-        <!-- <li>
-          <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-            <i class="bi bi-person"></i>
-            <span>My Profile</span>
-          </a>
-        </li> -->
-        <!-- <li>
-          <hr class="dropdown-divider">
-        </li>
+        </ul><!-- End Profile Dropdown Items -->
+      </li><!-- End Profile Nav -->
 
-        <li>
-          <a class="dropdown-item d-flex align-items-center" href="http://localhost/php-lfis/admin?page=user">
-            <i class="bi bi-gear"></i>
-            <span>Account Settings</span>
-          </a>
-        </li>
-        <li>
-          <hr class="dropdown-divider">
-        </li>
-       
-        <li>
-          <hr class="dropdown-divider">
-        </li> -->
-
-        <li>
-          <a class="dropdown-item d-flex align-items-center" href="<?= base_url ?>/logout.php">
-            <i class="bi bi-box-arrow-right"></i>
-            <span>登出</span>
-          </a>
-        </li>
-
-      </ul><!-- End Profile Dropdown Items -->
-    </li><!-- End Profile Nav -->
-
-  </ul>
-</nav><!-- End Icons Navigation -->
+    </ul>
+  </nav><!-- End Icons Navigation -->
 
 </header>
 
-<aside id="sidebar" class="sidebar">
-<ul class="sidebar-nav" id="sidebar-nav">
+<body>
+  <aside id="sidebar" class="sidebar">
+  <ul class="sidebar-nav" id="sidebar-nav">
 
-  <li class="nav-item">
-    <a class="nav-link " href="<?= base_url ?>/admin">
-      <i class="bi bi-house"></i>
-      <span>首頁</span>
-    </a>
-  </li>
+    <li class="nav-item">
+      <a class="nav-link " href="<?= base_url ?>admin">
+        <i class="bi bi-house"></i>
+        <span>首頁</span>
+      </a>
+    </li>
 
-  <li class="nav-item">
-    <a class="nav-link collapsed" data-bs-target="#user-nav" data-bs-toggle="collapse" href="#" data-bs-collapse="false">
-      <i class="bi bi-person"></i><span>用戶管理</span>
-                  
-      <i class="bi bi-chevron-down ms-auto"></i>
-    </a>
-    <ul id="user-nav" class="nav-content collapse  " data-bs-parent="#sidebar-nav">
-      <li>
-        <a href="<?= base_url ?>/admin/user/index.php" class="">
-          <span>用戶清單</span>
-        </a>
-      </li>
-      <li>
-        <a href="<?= base_url ?>/admin/user/insert_admin.php" class="">
-          <span>新增管理員</span>
-        </a>
-      </li>
-    </ul>
-  </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#user-nav" data-bs-toggle="collapse" href="#" data-bs-collapse="false">
+        <i class="bi bi-person"></i><span>用戶管理</span>
+                    
+        <i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="user-nav" class="nav-content collapse  " data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="<?= base_url ?>admin/user/index.php" class="">
+            <span>用戶清單</span>
+          </a>
+        </li>
+        <li>
+          <a href="<?= base_url ?>admin/user/insert_admin.php" class="">
+            <span>新增管理員</span>
+          </a>
+        </li>
+      </ul>
+    </li>
 
-  <li class="nav-item">
-    <a class="nav-link collapsed" data-bs-target="#dept-nav" data-bs-toggle="collapse" href="#" data-bs-collapse="false">
-      <i class="bi bi-buildings"></i><span>場所管理</span>
-                  
-      <i class="bi bi-chevron-down ms-auto"></i>
-    </a>
-    <ul id="dept-nav" class="nav-content collapse  " data-bs-parent="#sidebar-nav">
-      <li>
-        <a href="<?= base_url ?>/admin/dept/index.php" class="">
-          <span>場所清單</span>
-        </a>
-      </li>
-      <li>
-        <a href="<?= base_url ?>/admin/dept/insert.php" class="">
-          <span>新增場所</span>
-        </a>
-      </li>
-    </ul>
-  </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#dept-nav" data-bs-toggle="collapse" href="#" data-bs-collapse="false">
+        <i class="bi bi-buildings"></i><span>場所管理</span>
+                    
+        <i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="dept-nav" class="nav-content collapse  " data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="<?= base_url ?>admin/dept/index.php" class="">
+            <span>場所清單</span>
+          </a>
+        </li>
+        <li>
+          <a href="<?= base_url ?>admin/dept/insert.php" class="">
+            <span>新增場所</span>
+          </a>
+        </li>
+      </ul>
+    </li>
 
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="<?= base_url ?>/admin/post">
-      <i class="bi bi-newspaper"></i>
-      <span>貼文管理</span>
-    </a>
-  </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="<?= base_url ?>admin/post">
+        <i class="bi bi-newspaper"></i>
+        <span>貼文管理</span>
+      </a>
+    </li>
 
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="<?= base_url ?>/admin/item">
-      <i class="bi bi-phone"></i>
-      <span>物品管理</span>
-    </a>
-  </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="<?= base_url ?>admin/item">
+        <i class="bi bi-phone"></i>
+        <span>物品管理</span>
+      </a>
+    </li>
+    
+    <!-- <li class="nav-item">
+      <a class="nav-link collapsed" href="<?= base_url ?>">
+        <i class="bi bi-escape"></i>
+        <span>登出</span>
+      </a>
+    </li> -->
+  </ul>
 
-    <!-- <li class="nav-heading">Maintenance</li>
+  </aside>
 
-  <li class="nav-item">
-    <a class="nav-link collapsed nav-users" href="http://localhost/php-lfis/admin?page=user/list">
-      <i class="bi bi-people"></i>
-      <span>Users</span>
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link collapsed  nav-system_info" href="http://localhost/php-lfis/admin?page=system_info/contact_information">
-      <i class="bi bi-telephone"></i>
-      <span>Contact Information</span>
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link collapsed  nav-system_info" href="http://localhost/php-lfis/admin?page=system_info">
-      <i class="bi bi-gear"></i>
-      <span>System Information</span>
-    </a>
-  </li> -->
-  
-  <!-- <li class="nav-item">
-    <a class="nav-link collapsed" href="<?= base_url ?>">
-      <i class="bi bi-escape"></i>
-      <span>登出</span>
-    </a>
-  </li>
-</ul> -->
+  <main id="main" class="main">
+    <div class="pagetitle" style="margin-bottom: 25px;">
+      <h1>首頁</h1>
+      <hr>
+    </div>
+    
+    <section class="section dashboard">
+    <div class="row">
 
-</aside>
+      <!-- Left side columns -->
+      <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+        <div class="row">
+
+          <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+            <div class="card info-card">
+              <div class="card-body">
+                <h5 class="card-title"><strong>用戶數量</strong></h5>
+
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center bg-success bg-opacity-25 text-success">
+                    <i class="bi bi-person"></i>
+                  </div>
+                  <div class="ps-3">
+                    <?php 
+                    	include '../conn.php';
+
+                      $sql = "SELECT count(ID) AS count
+                        FROM user
+                        WHERE type = 0;";
+                      $result = $conn->query($sql);
+                      $row = $result->fetch_assoc();
+                      echo "管理員\t<h6 style='display: inline'>" . $row['count'] . "</h6>";
+                      $sql = "SELECT count(ID) AS count
+                      FROM user
+                      WHERE type = 1;";
+                      $result = $conn->query($sql);
+                      $row = $result->fetch_assoc();
+                      echo "<span style='margin-left: 5px; margin-left: 20px;margin-right: 20px;'>|</span>使用者\t<h6 style='display: inline'>" . $row['count'] . "</h6>";
+                    ?>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+            <div class="card info-card">
+              <div class="card-body">
+                <h5 class="card-title"><strong>貼文數量</strong></h5>
+
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center bg-dark bg-opacity-25 text-dark">
+                    <i class="bi bi-newspaper"></i>
+                  </div>
+                  <div class="ps-3">
+                    <?php 
+                      $sql = "SELECT count(ID) AS count
+                        FROM post
+                        WHERE type = 0;";
+                      $result = $conn->query($sql);
+                      $row = $result->fetch_assoc();
+                      echo "失物尋找\t<h6 style='display: inline'>" . $row['count'] . "</h6>";
+                      $sql = "SELECT count(ID) AS count
+                      FROM post
+                      WHERE type = 1;";
+                      $result = $conn->query($sql);
+                      $row = $result->fetch_assoc();
+                      echo "<span style='margin-left: 5px; margin-left: 20px;margin-right: 20px;'>|</span>拾獲招領\t<h6 style='display: inline'>" . $row['count'] . "</h6>";
+                    ?>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+            <div class="card info-card">
+              <div class="card-body">
+                <h5 class="card-title"><strong>場所數量</strong></h5>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center bg-dark bg-opacity-25 text-dark">
+                    <i class="bi bi-building"></i>
+                  </div>
+                  <div class="ps-3">
+                    <?php 
+                      $sql = "SELECT count(ID) AS count
+                        FROM department;";
+                      $result = $conn->query($sql);
+                      $row = $result->fetch_assoc();
+                      echo "<h6 style='display: inline'>" . $row['count'] . "</h6>";
+                    ?>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+            <div class="card info-card">
+              <div class="card-body">
+                <h5 class="card-title"><strong>物品數量</strong></h5>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center bg-primary bg-opacity-25 text-primary">
+                    <i class="bi bi-phone"></i>
+                  </div>
+                  <div class="ps-3">
+                    <?php 
+                      $sql = "SELECT count(ID) AS count
+                        FROM item
+                        WHERE state = 0;";
+                      $result = $conn->query($sql);
+                      $row = $result->fetch_assoc();
+                      echo "尋找中\t<h6 style='display: inline'>" . $row['count'] . "</h6>";
+                      $sql = "SELECT count(ID) AS count
+                        FROM item
+                        WHERE state = 1;";
+                      $result = $conn->query($sql);
+                      $row = $result->fetch_assoc();
+                      echo "<span style='margin-left: 5px; margin-left: 20px;margin-right: 20px;'>|</span>　待領取\t<h6 style='display: inline'>" . $row['count'] . "</h6>";
+                      $sql = "SELECT count(ID) AS count
+                        FROM item
+                        WHERE state = 2;";
+                      $result = $conn->query($sql);
+                      $row = $result->fetch_assoc();
+                      echo "<span style='margin-left: 5px; margin-left: 20px;margin-right: 20px;'>|</span>　已解決\t<h6 style='display: inline'>" . $row['count'] . "</h6>";
+                    ?>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+     
+
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title"></h5>
+                                                <div id="banner-slider" class="carousel slide" data-bs-ride="carousel">
+                  <div class="carousel-inner">
+                                        <div class="carousel-item">
+                      <img src="<?= base_url ?>uploads/banner/lf-wp-1.jpg" class="d-block w-100" alt="Banner Image 1">
+                    </div>
+                                        <div class="carousel-item active">
+                      <img src="<?= base_url ?>uploads/banner/lf-wp-2.jpg" class="d-block w-100" alt="Banner Image 2">
+                    </div>
+                                    </div>
+                
+
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </div><!-- End Left side columns -->
+
+    </div>
+  </div></section>      </main>
+
+  <script>
+        // JavaScript 代碼部分
+        document.querySelector('.toggle-sidebar-btn').addEventListener('click', function() {
+            document.body.classList.toggle('toggle-sidebar');
+        });
+    </script>
+</body>
